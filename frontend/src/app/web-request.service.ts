6671c606473920697ac9bcae75ca3e05d757c549
gsak3l <gsakel5@gmail.com>
2020-08-12 20:58:08 +0300

    WebRequestService done

---
+++ frontend/src/app/web-request.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebRequestService {
+
+  readonly ROOT_URL;
+
+  constructor(private http: HttpClient) {
+    this.ROOT_URL = 'http://localhost:4200/';
+  };
+
+  // This is Used to Send Get Requests to the Database
+  get(uri: string) {
+    return this.http.get(`${this.ROOT_URL}/${uri}`);
+  };
+
+  // This is Used to Send Get Requests to the Database
+  post(uri: string, payload: Object) {
+    return this.http.post(`${this.ROOT_URL}/${uri}`, payload);
+  };
+  
+  // This is Used to Send Get Requests to the Database
+  patch(uri: string, payload: Object) {
+    return this.http.patch(`${this.ROOT_URL}/${uri}`, payload);
+  };
+
+  // This is Used to Send Get Requests to the Database
+  delete(uri: string) {
+    return this.http.delete(`${this.ROOT_URL}/${uri}`);
+  };
+
+}

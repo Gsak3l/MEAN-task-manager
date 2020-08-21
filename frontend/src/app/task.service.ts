2ea3439163506e50e1cd9f92beffe2f7377b62d6
gsak3l <gsakel5@gmail.com>
2020-08-21 22:29:15 +0300

    New Task Button is Working Now

--- frontend/src/app/task.service.ts
+++ frontend/src/app/task.service.ts
@@ -8,18 +8,23 @@ export class TaskService {
 
   constructor(private webReqService: WebRequestService) { }
 
-  createList(title: string) {
-    // This Should Send a Web Request to Create a New List
-    return this.webReqService.post('lists', { title });
-  }
-
   getLists() {
     // This Should Send a Web Requests With all the Lists on the Database
     return this.webReqService.get('lists');
   }
 
+  createList(title: string) {
+    // This Should Send a Web Request to Create a New List
+    return this.webReqService.post('lists', { title });
+  }
+
   getTasks(listId: string) {
     // This Should Send a Web Requests With all the Tasks for all the Lists on the Database
     return this.webReqService.get(`lists/${listId}/tasks`);
+  }
+
+  createTask(title: string, listId: string) {
+    // This Should Send a Web Request to Create a New Task
+    return this.webReqService.post(`lists/${listId}/tasks`, { title });
   }
 }
